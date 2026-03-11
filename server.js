d553b7aa3aaac42f60489011a729ef647263f672
MAGV09 <mohamedadel2002320@gmail.com>
2026-03-11 09:05:56 +0200

    refactor: use express

--- server.js
+++ server.js
@@ -1,43 +1,25 @@
-const http = require("node:http");
-const fs = require("node:fs");
-const fsPromises= require('node:fs/promises')
-const { pipeline } = require("node:stream/promises");
-const server = http.createServer(async (req, res) => {
-  res.setHeader("Content-Type", "text/html");
-  let path = "./views/";
-  switch (req.url) {
-    case "/":
-      path += "index.html";
-      break;
-    case "/about":
-      path += "about.html";
-      break;
-    case "/contact":
-      path += "contactMe.html";
-      break;
-    case "/contact-me":
-      res.statusCode = 301;
-      res.setHeader("Location", "/contact");
-      res.end();
-      return;
-      break;
-    default:
-      path += "404.html";
-      res.statusCode = 404;
-  }
+const express = require("express");
+const app = express();
+const path = require("node:path");
 
-  try {
-    //stream approach
-    await pipeline(fs.createReadStream(path), res);
- 
-    //normal read approach
-    // const file = await fsPromises.readFile(path)
-    // res.end(file)
-  } catch (err) {
-    console.log(`you have a problem mate ${err}`);
-  }
+app.listen(3000);
+
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: path.join(__dirname, "views") });
 });
 
-server.listen(3000, () => {
-  console.log("server listening on port 3000");
+app.get("/about", (req, res) => {
+  res.sendFile("about.html", { root: path.join(__dirname, "views") });
+});
+
+app.get("/contact", (req, res) => {
+  res.sendFile("contactMe.html", { root: path.join(__dirname, "views") });
+});
+
+app.get("/contact-me", (req, res) => {
+  res.redirect("/contact");
+});
+
+app.use((req, res) => {
+  res.status(404).sendFile("404.html", { root: path.join(__dirname, "views") });
 });
